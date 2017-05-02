ad33a0fc4d26713704267d6d1d4efe17ba23cd29
David Scales <dscales@google.com>
2017-05-02 16:49:50 -0700

    moves images to top level

--- main.js
+++ main.js
@@ -9,15 +9,15 @@ if (!shell.which('git')) {
 }
 
 var languages = ['en', 'sp'];
-//
-// // Pull GDoc files as markdown
-// shell.exec('./claat update');
-//
-// // Clean up markdown
-// var filesToProcess = glob.find('**/index.md');
-// filesToProcess.forEach(function(filename) {
-//   cleanup.cleanup(filename, filename, '');
-// });
+
+// Pull GDoc files as markdown
+shell.exec('./claat update');
+
+// Clean up markdown
+var filesToProcess = glob.find('**/index.md');
+filesToProcess.forEach(function(filename) {
+  cleanup.cleanup(filename, filename, '');
+});
 
 // Push course packages
 for (var courseTitle in courses) {
@@ -26,35 +26,41 @@ for (var courseTitle in courses) {
   shell.cd(courseTitle);
   var course = courses[courseTitle];
   if (course.type == 'code') {
-    // console.log('rm -rf $(ls)');
-    // shell.exec('rm -rf $(ls)');
-    // course.modules.forEach(function(module) {
-    //   console.log('cp -R ../en/code/' + module, './' + module);
-    //   shell.cp('-R', '../en/code/' + module, './' + module);
-    // });
+    console.log('rm -rf $(ls)');
+    shell.exec('rm -rf $(ls)');
+    course.modules.forEach(function(module) {
+      console.log('cp -R ../en/code/' + module, './' + module);
+      shell.cp('-R', '../en/code/' + module, './' + module);
+    });
   } else {
     console.log('mkdir img');
-    // shell.mkdir('img');
+    shell.mkdir('img');
     languages.forEach(function(lang) {
       var files = shell.ls('./' + lang);
       console.log('rm -rf ' + lang + '/*');
       shell.exec('rm -rf ' + lang + '/*');
       files.forEach(function(file) {
-        console.log('cp ../' + lang + '/' + course.type + '/' + file + '/index.md', './' + lang + '/' + file + '.md');
-        shell.cp('../' + lang + '/' + course.type + '/' + file + '/index.md', './' + lang + '/' + file + '.md');
+        console.log('cp ../' + lang + '/' + course.type + '/' + file +
+          '/index.md', './' + lang + '/' + file + '.md');
+        shell.cp('../' + lang + '/' + course.type + '/' + file +
+          '/index.md', './' + lang + '/' + file + '.md');
+        console.log('cp -R', '../' + lang + '/' + course.type + '/' + file +
+          '/img', './img');
+        shell.cp('-R', '../' + lang + '/' + course.type + '/' + file +
+          '/img', './');
       });
     });
   }
-  // console.log('echo "node_modules" > .gitignore');
-  // shell.exec('echo "node_modules" > .gitignore');
-  // console.log('echo ".DS_Store" >> .gitignore');
-  // shell.exec('echo ".DS_Store" >> .gitignore');
-  // console.log('git add . && git commit -m "autoupdate' + Date.now() +
-  //   '" && git push');
-  // shell.exec('git add . && git commit -m "autoupdate' + Date.now() +
-  //   '" && git push');
+  console.log('echo "node_modules" > .gitignore');
+  shell.exec('echo "node_modules" > .gitignore');
+  console.log('echo ".DS_Store" >> .gitignore');
+  shell.exec('echo ".DS_Store" >> .gitignore');
+  console.log('git add . && git commit -m "autoupdate' + Date.now() +
+    '" && git push');
+  shell.exec('git add . && git commit -m "autoupdate' + Date.now() +
+    '" && git push');
   console.log('cd ..');
   shell.cd('..');
-  // console.log('rm -rf ' + courseTitle);
-  // shell.rm('-rf', courseTitle);
+  console.log('rm -rf ' + courseTitle);
+  shell.rm('-rf', courseTitle);
 }
