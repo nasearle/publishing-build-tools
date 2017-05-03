9465e2d3b34d9b8dd08a13a26febec7e83508333
David Scales <dscales@google.com>
2017-05-03 14:48:49 -0700

    docs: add logs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var cleanup = require('./cleanup');
 var glob = require('globule');
 var shell = require('shelljs');
-var courses = require('./courses.json');
+var groupings = require('./groupings.json');
 var fs = require('fs');
 
 if (!shell.which('git')) {
@@ -11,64 +11,70 @@ if (!shell.which('git')) {
 
 var languages = ['en', 'sp'];
 
-// Pull GDoc files as markdown
+console.log('\nUpdating codelabs & concepts markdown files from GDocs...\n');
 shell.exec('./claat update');
 
-// Clean up markdown
+console.log('\nCleaning codelabs & concepts markdown files...\n');
 var filesToProcess = glob.find('**/index.md');
 filesToProcess.forEach(function(filename) {
   cleanup.cleanup(filename, filename, '');
 });
 
-// Push course packages
-for (var courseTitle in courses) {
-  shell.exec('git clone https://github.com/nasearle/' + courseTitle + '.git');
-  console.log('cd ' + courseTitle);
-  shell.cd(courseTitle);
-  var course = courses[courseTitle];
-  if (course.type == 'code') {
-    course.modules.forEach(function(module) {
-      console.log('rm -rf ' + module);
+for (var groupingName in groupings) {
+  console.log('\n');
+  shell.exec('git clone https://github.com/nasearle/' + groupingName + '.git');
+  // console.log('cd ' + groupingName);
+  shell.cd(groupingName);
+  var grouping = groupings[groupingName];
+  if (grouping.type == 'code') {
+    console.log('\nUpdating course: ' + groupingName + '...\n');
+    grouping.modules.forEach(function(module) {
+      // console.log('rm -rf ' + module);
       shell.exec('rm -rf ' + module);
-      console.log('cp -R ../en/code/' + module, './' + module);
+      // console.log('cp -R ../en/code/' + module, './' + module);
       shell.cp('-R', '../en/code/' + module, './' + module);
     });
   } else {
-    console.log('rm -rf', 'img');
+    console.log('\nUpdating GitBook repo: ' + groupingName + '...\n');
+    // console.log('rm -rf', 'img');
     shell.rm('-rf', 'img');
-    console.log('mkdir img');
+    // console.log('mkdir img');
     shell.mkdir('img');
     if (!fs.existsSync('./README.md')) {
-      console.log('echo "readme" > README.md');
+      // console.log('echo "readme" > README.md');
+      console.log('No README.md exists (required for GitBook), writing...\n');
       shell.exec('echo "readme" > README.md');
     }
     languages.forEach(function(lang) {
+      console.log('Processing ' + lang + '...');
       var files = shell.ls('./' + lang);
-      console.log('rm -rf ' + lang + '/*');
+      // console.log('rm -rf ' + lang + '/*');
       shell.exec('rm -rf ' + lang + '/*');
       files.forEach(function(fileName) {
         var folderName = fileName.slice(0, -3); // remove file's .md extension
-        console.log('cp ../' + lang + '/' + course.type + '/' + folderName +
+        //  console.log('cp ../' + lang + '/' + grouping.type + '/' + folderName +
+        //    '/index.md', './' + lang + '/' + fileName);
+        shell.cp('../' + lang + '/' + grouping.type + '/' + folderName +
           '/index.md', './' + lang + '/' + fileName);
-        shell.cp('../' + lang + '/' + course.type + '/' + folderName +
-          '/index.md', './' + lang + '/' + fileName);
-        console.log('cp -R', '../' + lang + '/' + course.type + '/' +
-          folderName + '/img', './img');
-        shell.cp('-R', '../' + lang + '/' + course.type + '/' +
+        // console.log('cp -R', '../' + lang + '/' + grouping.type + '/' +
+        //   folderName + '/img', './img');
+        shell.cp('-R', '../' + lang + '/' + grouping.type + '/' +
           folderName + '/img', './');
       });
     });
   }
   if (!fs.existsSync('./.gitignore')) {
-    console.log('echo "node_modules\n.DS_Store" > .gitignore');
+    // console.log('echo "node_modules\n.DS_Store" > .gitignore');
+    console.log('No .gitignore exists, writing...\n');
     shell.exec('echo "node_modules\n.DS_Store" > .gitignore');
   }
-  console.log('git add . && git commit -m "autoupdate' + Date.now() +
-    '" && git push');
+  // console.log('git add . && git commit -m "autoupdate' + Date.now() +
+  //   '" && git push');
+  console.log('\nPushing changes to repo: ' + groupingName + '...\n');
   shell.exec('git add . && git commit -m "autoupdate' + Date.now() +
     '" && git push');
-  console.log('cd ..');
+  // console.log('cd ..');
   shell.cd('..');
-  console.log('rm -rf ' + courseTitle);
-  shell.rm('-rf', courseTitle);
+  // console.log('rm -rf ' + groupingName);
+  shell.rm('-rf', groupingName);
 }
