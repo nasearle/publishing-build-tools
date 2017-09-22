5800efc5d0dd9ee5bf0934281c5d84aeec036a9d
Nikita Gamolsky <ngamolsky@ngamolsky-macbookpro.roam.corp.google.com>
2017-09-22 13:42:54 -0700

    Remove unneeded eslinter file, fix build script and add package-lock.json

--- build-book.js
+++ build-book.js
@@ -5,11 +5,11 @@ const shell = require('shelljs')
 const globby = require('globby')
 const rimraf = require('rimraf')
 
-var currentPath = '.'
+var currentPath = process.cwd()
 
 // Get config file
 try {
-  var config = fs.readFileSync(process.cwd().concat('/config.json'))
+  var config = fs.readFileSync(currentPath.concat('/config.json'))
   // Remove all book content files (in case anything was deleted)
   globby.sync(['*', '!config.json']).forEach(function (item) {
     rimraf.sync(item)
@@ -22,8 +22,8 @@ try {
   updateBook(config)
   createSummary(config)
 } catch (err) {
-  console.log(err);
-  console.log(`No config file found in working directory (${process.cwd()})`)
+  console.log(err)
+  console.log(`No config file found in working directory (${currentPath})`)
   shell.exit(1)
 }
 
@@ -36,15 +36,16 @@ function updateBookRecursively (jsonArray) {
     if (jsonObject.hasOwnProperty('contents')) {
       currentPath = currentPath.concat('/'.concat(jsonObject.name))
       if (!fs.existsSync(currentPath)) {
-        console.log(`Created folder ${currentPath}`);
+        console.log(`Created folder ${currentPath}`)
         fs.mkdirSync(currentPath)
       }
+      console.log(currentPath)
       updateBookRecursively(jsonObject.contents)
       currentPath = currentPath.substring(0, currentPath.lastIndexOf('/'))
     }
 
     if (jsonObject.hasOwnProperty('id')) {
-      console.log(`Updating ${jsonObject.name}`);
+      console.log(`Updating ${jsonObject.name}`)
       shell.exec(`${__dirname}/claat export  -f md -o "${currentPath}" ${jsonObject.id}`)
     }
   })
