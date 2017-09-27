e8ee2c67924d401a7044b780e637fefac836d6a0
Nicolas Searle <nsearle@google.com>
2017-09-27 16:09:03 -0700

    added checks for .git to publishBook and createBook

--- createBook.js
+++ createBook.js
@@ -7,6 +7,7 @@ const glob = require('globule');
 const gutil = require('gulp-util');
 const shell = require('shelljs');
 const fs = require('fs');
+const chalk = require('chalk');
 
 let currentPath = process.cwd();
 
@@ -14,16 +15,23 @@ try {
   let config = fs.readFileSync(currentPath.concat('/config.json'));
   config = JSON.parse(config);
 
-  shell.exec('git init');
-  shell.exec(`git remote add origin ${config.gitbookRemote}`);
-  shell.exec('git pull origin master');
-  shell.exec('git branch --set-upstream-to=origin/master master');
+  if (!fs.existsSync(`${currentPath}/.git`)) {
+    shell.exec('git init');
+    shell.exec(`git remote add origin ${config.gitbookRemote}`);
+    shell.exec('git pull origin master');
+    shell.exec('git branch --set-upstream-to=origin/master master');
 
-  shell.cp('-r', `${__dirname}/starting-files/styles`, `${currentPath}/`);
-  shell.cp(`${__dirname}/starting-files/book.json`, `${currentPath}/`);
+    shell.cp('-r', `${__dirname}/starting-files/styles`, `${currentPath}/`);
+    shell.cp(`${__dirname}/starting-files/book.json`, `${currentPath}/`);
 
-  shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
-               '" && git push --set-upstream origin master');
+    shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
+                 '" && git push --set-upstream origin master');
+  } else {
+    gutil.log(
+      chalk.red('Current directory is already initialized as a Git repo!'));
+    gutil.log(
+      chalk.red('You may be looking for the publish-book command'));
+  }
 } catch (err) {
   console.log(err);
   shell.exit(1);
--- publishBook.js
+++ publishBook.js
@@ -8,37 +8,30 @@ const gutil = require('gulp-util');
 const shell = require('shelljs');
 const fs = require('fs');
 const chalk = require('chalk');
-const gitConfig = require('git-config');
 
 let currentPath = process.cwd();
 
-try {
-  let config = fs.readFileSync(currentPath.concat('/config.json'));
-  config = JSON.parse(config);
-  download.updateBook(config);
+if (fs.existsSync(`${currentPath}/.git`)) {
+  try {
+    let config = fs.readFileSync(currentPath.concat('/config.json'));
+    config = JSON.parse(config);
+    download.updateBook(config);
 
-  gutil.log(chalk.cyan('Cleaning'), chalk.cyan(config.title));
-  let filesToProcess = glob.find('**/index.md');
-  filesToProcess.forEach(function(filename) {
-    cleanBook.cleanup(filename, filename);
-  });
-  gutil.log(chalk.yellow('-->'), chalk.cyan('Cleaned'), chalk.cyan(config.title));
+    gutil.log(chalk.cyan('Cleaning'), chalk.cyan(config.title));
+    let filesToProcess = glob.find('**/index.md');
+    filesToProcess.forEach(function(filename) {
+      cleanBook.cleanup(filename, filename);
+    });
+    gutil.log(chalk.yellow('-->'), chalk.cyan('Cleaned'), chalk.cyan(config.title));
 
-  createSummary.createSummary(config);
+    createSummary.createSummary(config);
 
-  // var config = gitConfig.sync();
-  // console.log(config);
-
-  // if (fs.existsSync('.git')) {
-  //   shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
-  //                '" && git push');
-  // } else {
-  //   gutil.log(chalk.red('Not a git repository!'), chalk.red('Please run create-book'));
-  // }
-
-  shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
-               '" && git push');
-} catch (err) {
-  console.log(err);
-  shell.exit(1);
+    shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
+                 '" && git push');
+  } catch (err) {
+    console.log(err);
+    shell.exit(1);
+  }
+} else {
+  gutil.log(chalk.red('Not a git repository!'), chalk.red('Please run create-book'));
 }
