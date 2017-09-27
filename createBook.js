f43ef243bc2432bdc74123288b010f62b86d81e5
Nicolas Searle <nsearle@google.com>
2017-09-27 16:12:34 -0700

    made createBook consistent with publishBook

--- createBook.js
+++ createBook.js
@@ -11,28 +11,29 @@ const chalk = require('chalk');
 
 let currentPath = process.cwd();
 
-try {
-  let config = fs.readFileSync(currentPath.concat('/config.json'));
-  config = JSON.parse(config);
+if (!fs.existsSync(`${currentPath}/.git`)) {
+  try {
+    let config = fs.readFileSync(currentPath.concat('/config.json'));
+    config = JSON.parse(config);
 
-  if (!fs.existsSync(`${currentPath}/.git`)) {
-    shell.exec('git init');
-    shell.exec(`git remote add origin ${config.gitbookRemote}`);
-    shell.exec('git pull origin master');
-    shell.exec('git branch --set-upstream-to=origin/master master');
+      shell.exec('git init');
+      shell.exec(`git remote add origin ${config.gitbookRemote}`);
+      shell.exec('git pull origin master');
+      shell.exec('git branch --set-upstream-to=origin/master master');
 
-    shell.cp('-r', `${__dirname}/starting-files/styles`, `${currentPath}/`);
-    shell.cp(`${__dirname}/starting-files/book.json`, `${currentPath}/`);
+      shell.cp('-r', `${__dirname}/starting-files/styles`, `${currentPath}/`);
+      shell.cp(`${__dirname}/starting-files/book.json`, `${currentPath}/`);
 
-    shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
-                 '" && git push --set-upstream origin master');
-  } else {
-    gutil.log(
-      chalk.red('Current directory is already initialized as a Git repo!'));
-    gutil.log(
-      chalk.red('You may be looking for the publish-book command'));
+      shell.exec('git add . && git commit -m "autoupdate-' + Date.now() +
+                   '" && git push --set-upstream origin master');
+
+  } catch (err) {
+    console.log(err);
+    shell.exit(1);
   }
-} catch (err) {
-  console.log(err);
-  shell.exit(1);
+} else {
+  gutil.log(
+    chalk.red('Current directory is already initialized as a Git repo!'));
+  gutil.log(
+    chalk.red('You may be looking for the publish-book command'));
 }
